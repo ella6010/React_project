958761295a76f0ab99a0fa338f3a05eaa8e63412
Ella <ella6010@gmail.com>
2025-04-09 15:37:51 +0900

    react 6회차 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <header>
         <h1>💚 동물 조아 💚</h1>
       </header>
+      <Routes>
+        <Route path="" element></Route>
+      </Routes>
       <footer>all rights reserved to OZ</footer>
     </>
   );
