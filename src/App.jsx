94368d2f1c009c5505cb00925afdf0924321a05a
Ella <ella6010@gmail.com>
2025-04-09 15:49:42 +0900

    react 6회차 실습

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Detail from "./page/Detail";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         <h1>💚 동물 조아 💚</h1>
       </header>
       <Routes>
-        <Route path="" element></Route>
+        <Route path='/' element={<Main /> }></Route>
+        <Route path='/detail' element={<Detail />}></Route>
+        <Route path='/search' element={<Search />}></Route>
       </Routes>
       <footer>all rights reserved to OZ</footer>
     </>
